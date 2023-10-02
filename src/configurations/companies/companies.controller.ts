7e68d69e74b00c74667fffca16b49ada9805b47c
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-02 07:29:25 +0300

    feat: Add authentication guards and permissions to CompaniesController
    This commit enhances the CompaniesController by adding authentication guards and permissions using NestJS decorators and guards:
    Additionally, the `update` action now checks for an ID mismatch between the URL parameter and the request body to prevent potential data inconsistencies, throwing a `BadRequestException` if a mismatch is detected.
    
    These security enhancements ensure that only authenticated users with the appropriate permissions can perform CRUD operations on companies, improving the overall security of the application.

--- src/configurations/companies/companies.controller.ts
+++ src/configurations/companies/companies.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  BadRequestException,
+} from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 
+/** Guards and Decorators */
+import { AccessTokenGuard } from 'src/security/auth/guards/access-token.guard';
+import { PermissionsGuard } from 'src/security/auth/guards/permissions.guard';
+import { Permissions } from 'src/security/auth/decorators/permissions.decorator';
+
+@UseGuards(AccessTokenGuard, PermissionsGuard)
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companiesService.create(createCompanyDto);
+  @Permissions('add_company')
+  async create(@Body() createCompanyDto: CreateCompanyDto) {
+    return await this.companiesService.create(createCompanyDto);
   }
 
   @Get()
-  findAll() {
-    return this.companiesService.findAll();
+  @Permissions('view_company')
+  async findAll() {
+    return await this.companiesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
+  @Permissions('view_company')
+  async findOne(@Param('id') id: number) {
+    return await this.companiesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companiesService.update(+id, updateCompanyDto);
+  @Permissions('change_company')
+  async update(
+    @Param('id') id: number,
+    @Body() updateCompanyDto: UpdateCompanyDto,
+  ) {
+    if (id != updateCompanyDto.id)
+      throw new BadRequestException('ID mismatch between URL and request body');
+
+    return await this.companiesService.update(+id, updateCompanyDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+  @Permissions('delete_company')
+  async remove(@Param('id') id: number) {
+    return await this.companiesService.remove(+id);
   }
 }
