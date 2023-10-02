e404b16d36f1320ccd56dd084948febe0de46795
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-02 07:28:29 +0300

    refactor: enhance CompaniesService with TypeORM
    Revised the 'CompaniesService' to utilize TypeORM for interacting with the database. The service now leverages the repository pattern to perform CRUD operations on the 'Company' entity. This update improves data access and management within the service.

--- src/configurations/companies/companies.service.ts
+++ src/configurations/companies/companies.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+/** DTOs */
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 
+/** Entities */
+import { Company } from './entities/company.entity';
+
 @Injectable()
 export class CompaniesService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+  constructor(
+    @InjectRepository(Company)
+    private readonly companyRepository: Repository<Company>,
+  ) {}
+
+  async create(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    const company = this.companyRepository.create(createCompanyDto);
+    return await this.companyRepository.save(company);
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(): Promise<Company[]> {
+    return await this.companyRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
+  async findOne(id: number): Promise<Company> {
+    return await this.companyRepository.findOneBy({ id });
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async findByName(name: string): Promise<Company> {
+    return await this.companyRepository.findOne({ where: { name } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async update(
+    id: number,
+    updateCompanyDto: UpdateCompanyDto,
+  ): Promise<Company> {
+    const company = await this.findOne(id);
+    if (!company) throw new NotFoundException('Company not found.');
+
+    Object.assign(company, updateCompanyDto);
+    return await this.companyRepository.save(company);
+  }
+
+  async remove(id: number): Promise<void> {
+    const company = await this.findOne(id);
+    if (!company) throw new NotFoundException('Company not found.');
+
+    await this.companyRepository.remove(company);
   }
 }
