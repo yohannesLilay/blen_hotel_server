c91836254a6e345ea66b4328bac82ad145262ae3
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-02 07:11:31 +0300

    feat: generate companies resource
    Generated the companies resource to handle companies.

--- src/configurations/configurations.module.ts
+++ src/configurations/configurations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { CategoriesModule } from './categories/categories.module';
 import { InventoriesModule } from './inventories/inventories.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [SuppliersModule, CategoriesModule, InventoriesModule],
+  imports: [SuppliersModule, CategoriesModule, InventoriesModule, CompaniesModule],
   providers: [],
   exports: [SuppliersModule, CategoriesModule, InventoriesModule],
 })
