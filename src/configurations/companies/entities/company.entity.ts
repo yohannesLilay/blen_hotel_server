8b2c99efb93c0381a418ca2d98d1074e59effeb4
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-02 07:26:44 +0300

    feat: Enhance Company entity with columns
    Updated the 'Company' entity class to include additional columns using TypeORM decorators.

--- src/configurations/companies/entities/company.entity.ts
+++ src/configurations/companies/entities/company.entity.ts
@@ -1,1 +1,29 @@
-export class Company {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Company {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 200, nullable: true })
+  description: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
