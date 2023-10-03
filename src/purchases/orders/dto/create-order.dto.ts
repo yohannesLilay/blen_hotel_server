64b19b8b8dda75c37c87f33d9cf89588ed168583
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-03 13:18:39 +0300

    feat: Add validation decorators to CreateOrderDto & UpdateOrderDto
    This commit enhances the CreateOrderDto class by adding validation decorators from class-validator library. These decorators ensure that the incoming data adheres to specific constraints and validation rules:
    These validations improve the data integrity and reliability of the CreateOrderDto input, ensuring that only valid data is accepted when creating a new sub order.

--- src/purchases/orders/dto/create-order.dto.ts
+++ src/purchases/orders/dto/create-order.dto.ts
@@ -1,1 +1,50 @@
-export class CreateOrderDto {}
+import {
+  IsArray,
+  IsDateString,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Validate,
+  ValidateNested,
+} from 'class-validator';
+import { UniqueOrderNumberValidator } from '../validators/unique-order-number.validator';
+import { ValidProductValidator } from '../validators/valid-product.validator';
+import { Type } from 'class-transformer';
+
+class OrderItemDto {
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  unit_price: number;
+
+  @IsString()
+  @IsOptional()
+  remark?: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @Validate(ValidProductValidator)
+  product_id: number;
+}
+
+export class CreateOrderDto {
+  @IsString()
+  @IsNotEmpty()
+  @Validate(UniqueOrderNumberValidator)
+  order_number: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  order_date: Date;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+}
