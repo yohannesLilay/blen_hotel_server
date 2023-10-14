ed21fb4035121c951b5840276750d92898227b34
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-14 11:33:23 +0300

    refactor: Improve ProductsService with new features
    This commit refactors the ProductsService in the following ways:
    - Added new import statements for HttpException, HttpStatus, and XLSX.
    - Added a new method `importExcel` for importing data from an Excel file.
    - Added validation to check for the validity of the data while importing.

--- src/product-management/products/products.service.ts
+++ src/product-management/products/products.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as XLSX from 'xlsx';
 
 /** DTOs */
 import { CreateProductDto } from './dto/create-product.dto';
@@ -80,7 +86,7 @@ export class ProductsService {
   }
 
   async template() {
-    const categories = await this.categoriesService.findAll();
+    const categories = await this.categoriesService.findAllList();
     return { categoryOptions: categories, uomOptions: UnitOfMeasure };
   }
 
@@ -118,5 +124,132 @@ export class ProductsService {
     if (!product) throw new NotFoundException('Product not found.');
 
     await this.productRepository.remove(product);
+  }
+
+  async importExcel(file: Express.Multer.File): Promise<Product[]> {
+    const workbook = XLSX.read(file.buffer, { type: 'buffer' });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    const expectedHeaders = [
+      'name',
+      'unit_of_measure',
+      'category',
+      'stock_quantity',
+      'safety_stock_level',
+      'note',
+    ];
+
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+      defval: '',
+    });
+
+    if (!jsonData.length) {
+      throw new HttpException(
+        'No data found in the worksheet',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    if (
+      !expectedHeaders.every((header) => jsonData[0].hasOwnProperty(header))
+    ) {
+      throw new HttpException(
+        'Expected headers are missing in the Excel file',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    for (const row of jsonData) {
+      const name = row['name'].trim();
+      const unitOfMeasure = row['unit_of_measure'].trim();
+      const category = row['category'].trim();
+      const stockQuantity = row['stock_quantity'];
+      const safetyStockLevel = row['safety_stock_level'];
+
+      this.validateField(name, 'Product name cannot be empty or null.');
+      this.validateField(
+        unitOfMeasure,
+        'Product unit of measure cannot be empty or null.',
+      );
+      this.validateField(category, 'Product category cannot be empty or null.');
+
+      if (!Object.values(UnitOfMeasure).includes(unitOfMeasure)) {
+        throw new HttpException(
+          'Invalid unit of measure value.',
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+      }
+
+      if (await this.doesProductNameExist(name)) {
+        throw new HttpException(
+          `Product with name '${name}' already exists.`,
+          HttpStatus.CONFLICT,
+        );
+      }
+
+      if (!(await this.isCategoryNameValid(category))) {
+        throw new HttpException(
+          `Product Category with name '${category}' does not exists.`,
+          HttpStatus.CONFLICT,
+        );
+      }
+
+      if (
+        stockQuantity !== null &&
+        stockQuantity !== undefined &&
+        stockQuantity !== ''
+      ) {
+        const stockQuantityNumber = parseInt(stockQuantity);
+        if (isNaN(stockQuantityNumber)) {
+          throw new HttpException(
+            'Stock quantity must be a valid number.',
+            HttpStatus.UNPROCESSABLE_ENTITY,
+          );
+        }
+      }
+
+      if (
+        safetyStockLevel !== null &&
+        safetyStockLevel !== undefined &&
+        safetyStockLevel !== ''
+      ) {
+        const safetyStockLevelNumber = parseInt(safetyStockLevel);
+        if (isNaN(safetyStockLevelNumber)) {
+          throw new HttpException(
+            'Safety stock level must be a valid number.',
+            HttpStatus.UNPROCESSABLE_ENTITY,
+          );
+        }
+      }
+
+      // Modify the row to use the Category model
+      row['category'] = await this.categoriesService.findByName(category);
+      row['stock_quantity'] = stockQuantity ? stockQuantity : 0;
+      row['safety_stock_level'] = safetyStockLevel ? safetyStockLevel : null;
+      row['name'] = name;
+      row['unit_of_measure'] = unitOfMeasure;
+    }
+
+    return await this.productRepository.save(jsonData);
+  }
+
+  async doesProductNameExist(name): Promise<boolean> {
+    const product = await this.productRepository.findOne({
+      where: { name },
+    });
+
+    return !!product;
+  }
+
+  async isCategoryNameValid(name): Promise<boolean> {
+    const category = await this.categoriesService.findByName(name);
+
+    return !!category;
+  }
+
+  validateField(value, message) {
+    if (!value) {
+      throw new HttpException(message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 }
