e0aea3cc175f8dfc5bcfd7a0a10f38ef50606d15
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-25 14:23:49 +0300

    feat: Enhance Work Flow entity with columns
    Updated the 'WorkFlow' entity class to include additional columns using TypeORM decorators.

--- src/configurations/work-flows/entities/work-flow.entity.ts
+++ src/configurations/work-flows/entities/work-flow.entity.ts
@@ -1,1 +1,42 @@
-export class WorkFlow {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from 'src/security/roles/entities/role.entity';
+import { FlowType } from '../constants/flow-type.enum';
+import { FlowStep } from '../constants/flow-step.enum';
+
+@Entity()
+export class WorkFlow {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'enum', enum: FlowType })
+  flow_type: FlowType;
+
+  @Column({ type: 'enum', enum: FlowStep })
+  step: FlowStep;
+
+  @ManyToMany(() => Role, { nullable: true })
+  @JoinTable({
+    name: 'workflow_notify_to',
+    joinColumn: { name: 'work_flow_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
+  })
+  notify_to: Role[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
