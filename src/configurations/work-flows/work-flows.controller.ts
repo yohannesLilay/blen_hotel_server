4de2ca9bab6a19f94cd64cec7bde42a43fb810cb
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-25 14:33:31 +0300

    feat: Add access control and permissions to WorkFlowsController
    Updated the WorkFlowsController to include access control and permissions for each endpoint. The changes include the addition of guards, decorators, and permission checks to ensure that only authorized users can perform specific actions. The controller now uses AccessTokenGuard and PermissionsGuard to protect its endpoints.

--- src/configurations/work-flows/work-flows.controller.ts
+++ src/configurations/work-flows/work-flows.controller.ts
@@ -1,34 +1,68 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+  BadRequestException,
+} from '@nestjs/common';
 import { WorkFlowsService } from './work-flows.service';
 import { CreateWorkFlowDto } from './dto/create-work-flow.dto';
 import { UpdateWorkFlowDto } from './dto/update-work-flow.dto';
 
+/** Guards and Decorators */
+import { AccessTokenGuard } from 'src/security/auth/guards/access-token.guard';
+import { PermissionsGuard } from 'src/security/auth/guards/permissions.guard';
+import { Permissions } from 'src/security/auth/decorators/permissions.decorator';
+
+@UseGuards(AccessTokenGuard, PermissionsGuard)
 @Controller('work-flows')
 export class WorkFlowsController {
   constructor(private readonly workFlowsService: WorkFlowsService) {}
 
   @Post()
-  create(@Body() createWorkFlowDto: CreateWorkFlowDto) {
-    return this.workFlowsService.create(createWorkFlowDto);
+  @Permissions('add_work_flow')
+  async create(@Body() createWorkFlowDto: CreateWorkFlowDto) {
+    return await this.workFlowsService.create(createWorkFlowDto);
   }
 
   @Get()
-  findAll() {
-    return this.workFlowsService.findAll();
+  @Permissions('view_work_flow')
+  async findAll() {
+    return await this.workFlowsService.findAll();
+  }
+
+  @Get('template')
+  @Permissions('add_work_flow')
+  async template() {
+    return await this.workFlowsService.template();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.workFlowsService.findOne(+id);
+  @Permissions('view_work_flow')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.workFlowsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWorkFlowDto: UpdateWorkFlowDto) {
-    return this.workFlowsService.update(+id, updateWorkFlowDto);
+  @Permissions('change_work_flow')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateWorkFlowDto: UpdateWorkFlowDto,
+  ) {
+    if (id != updateWorkFlowDto.id)
+      throw new BadRequestException('ID mismatch between URL and request body');
+
+    return await this.workFlowsService.update(+id, updateWorkFlowDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.workFlowsService.remove(+id);
+  @Permissions('delete_work_flow')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.workFlowsService.remove(+id);
   }
 }
