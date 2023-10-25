10ad00a3a63eb66e25933787062fee84db717fe6
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-25 14:31:13 +0300

    feat: Add validation decorators to CreateWorkFlowDto & UpdateWorkFlowDto
    This commit enhances the CreateWorkFlowDto class by adding validation decorators from class-validator library. These decorators ensure that the incoming data adheres to specific constraints and validation rules:
    These validations improve the data integrity and reliability of the CreateWorkFlowDto input, ensuring that only valid data is accepted when creating a new work flow.

--- src/configurations/work-flows/dto/create-work-flow.dto.ts
+++ src/configurations/work-flows/dto/create-work-flow.dto.ts
@@ -1,1 +1,35 @@
-export class CreateWorkFlowDto {}
+import {
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  Min,
+  Validate,
+} from 'class-validator';
+import { FlowType } from '../constants/flow-type.enum';
+import { FlowStep } from '../constants/flow-step.enum';
+import { ValidRolesValidator } from '../validators/valid-roles.validator';
+import { UniqueWorkFlowValidator } from '../validators/unique-work-flow.validator';
+
+export class CreateWorkFlowDto {
+  @IsNotEmpty()
+  @IsEnum(FlowType)
+  flow_type: FlowType;
+
+  @IsNotEmpty()
+  @IsEnum(FlowStep)
+  @Validate(UniqueWorkFlowValidator)
+  step: FlowStep;
+
+  @IsOptional()
+  @IsArray({ message: 'Roles must be an array' })
+  @ArrayNotEmpty({ message: 'Roles array cannot be empty' })
+  @ArrayUnique({ message: 'Roles array cannot contain duplicate values' })
+  @IsInt({ each: true, message: 'Each role ID must be an integer' })
+  @Min(1, { each: true, message: 'Each role ID must be at least 1' })
+  @Validate(ValidRolesValidator)
+  notify_to: number[];
+}
--- src/configurations/work-flows/dto/update-work-flow.dto.ts
+++ src/configurations/work-flows/dto/update-work-flow.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateWorkFlowDto } from './create-work-flow.dto';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
-export class UpdateWorkFlowDto extends PartialType(CreateWorkFlowDto) {}
+export class UpdateWorkFlowDto extends PartialType(CreateWorkFlowDto) {
+  @IsInt()
+  @IsNotEmpty()
+  id: number;
+}
