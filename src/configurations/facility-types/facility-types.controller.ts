443c3a3d08a0ce2101b225a3589b988da6eae573
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 11:31:58 +0300

    feat: Add authentication guards and permissions to FacilityTypesController
    This commit enhances the FacilityTypesController by adding authentication guards and permissions using NestJS decorators and guards. Additionally, the `update` action now checks for an ID mismatch between the URL parameter and the request body to prevent potential data inconsistencies, throwing a `BadRequestException` if a mismatch is detected.
    
    These security enhancements ensure that only authenticated users with the appropriate permissions can perform CRUD operations on orders, improving the overall security of the application.

--- src/configurations/facility-types/facility-types.controller.ts
+++ src/configurations/facility-types/facility-types.controller.ts
@@ -1,34 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  BadRequestException,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { FacilityTypesService } from './facility-types.service';
 import { CreateFacilityTypeDto } from './dto/create-facility-type.dto';
 import { UpdateFacilityTypeDto } from './dto/update-facility-type.dto';
 
+/** Guards and Decorators */
+import { AccessTokenGuard } from 'src/security/auth/guards/access-token.guard';
+import { PermissionsGuard } from 'src/security/auth/guards/permissions.guard';
+import { Permissions } from 'src/security/auth/decorators/permissions.decorator';
+
+@UseGuards(AccessTokenGuard, PermissionsGuard)
 @Controller('facility-types')
 export class FacilityTypesController {
   constructor(private readonly facilityTypesService: FacilityTypesService) {}
 
   @Post()
-  create(@Body() createFacilityTypeDto: CreateFacilityTypeDto) {
-    return this.facilityTypesService.create(createFacilityTypeDto);
+  @Permissions('add_facility_type')
+  async create(@Body() createFacilityTypeDto: CreateFacilityTypeDto) {
+    return await this.facilityTypesService.create(createFacilityTypeDto);
   }
 
   @Get()
-  findAll() {
-    return this.facilityTypesService.findAll();
+  @Permissions('view_facility_type')
+  async findAll() {
+    return await this.facilityTypesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.facilityTypesService.findOne(+id);
+  @Permissions('view_facility_type')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.facilityTypesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFacilityTypeDto: UpdateFacilityTypeDto) {
-    return this.facilityTypesService.update(+id, updateFacilityTypeDto);
+  @Permissions('change_facility_type')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateFacilityTypeDto: UpdateFacilityTypeDto,
+  ) {
+    if (id != updateFacilityTypeDto.id)
+      throw new BadRequestException('ID mismatch between URL and request body');
+
+    return await this.facilityTypesService.update(+id, updateFacilityTypeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.facilityTypesService.remove(+id);
+  @Permissions('delete_facility_type')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.facilityTypesService.remove(+id);
   }
 }
