8d1695db6c948f8fe30597e60b2a8996fb703dcf
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 11:28:27 +0300

    feat: Enhance FacilityType entity with columns
    Updated the 'FacilityType' entity class to include additional columns using TypeORM decorators.

--- src/configurations/facility-types/entities/facility-type.entity.ts
+++ src/configurations/facility-types/entities/facility-type.entity.ts
@@ -1,1 +1,40 @@
-export class FacilityType {}
+import { Role } from 'src/security/roles/entities/role.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class FacilityType {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 200, nullable: true })
+  description: string;
+
+  @ManyToMany(() => Role, { cascade: true })
+  @JoinTable({
+    name: 'facility_type_roles',
+    joinColumn: { name: 'facility_type_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
+  })
+  responsible_roles: Role[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
