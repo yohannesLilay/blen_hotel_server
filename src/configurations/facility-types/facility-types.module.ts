bf3b9ab178fc73f043ae5d05369ab9bee495ccd0
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 11:33:16 +0300

    refactor: Update FacilityTypesModule structure and dependencies
    This commit refactors the structure and dependencies of the FacilityTypesModule:
    - Added TypeOrmModule for the FacilityType entity.
    - Imported necessary entities, modules, and custom validators.
    - Included UniqueNameValidator and ValidRolesValidator as providers.
    - Exported FacilityTypesService for external use.
    
    These changes enhance the organization and functionality of the module, improving maintainability and extensibility.

--- src/configurations/facility-types/facility-types.module.ts
+++ src/configurations/facility-types/facility-types.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/** Services */
 import { FacilityTypesService } from './facility-types.service';
+
+/** Controllers */
 import { FacilityTypesController } from './facility-types.controller';
 
+/** Entity */
+import { FacilityType } from './entities/facility-type.entity';
+
+/** Modules */
+import { RolesModule } from 'src/security/roles/roles.module';
+
+/** Custom Validators */
+import { UniqueNameValidator } from './validators/unique-name.validator';
+import { ValidRolesValidator } from './validators/valid-roles.validator';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([FacilityType]), RolesModule],
   controllers: [FacilityTypesController],
-  providers: [FacilityTypesService],
+  providers: [FacilityTypesService, UniqueNameValidator, ValidRolesValidator],
+  exports: [FacilityTypesService],
 })
 export class FacilityTypesModule {}
