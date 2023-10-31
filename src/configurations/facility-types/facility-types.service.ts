8ac10470a9f4ae9ed334abbdc5be77dd9883ed65
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 11:30:52 +0300

    refactor: enhance FacilityTypesService with TypeORM
    Revised the 'FacilityTypesService' to utilize TypeORM for interacting with the database. The service now leverages the repository pattern to perform operations on the 'FacilityType' entity. This update improves data access and management within the service.

--- src/configurations/facility-types/facility-types.service.ts
+++ src/configurations/facility-types/facility-types.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+/** DTOs */
 import { CreateFacilityTypeDto } from './dto/create-facility-type.dto';
 import { UpdateFacilityTypeDto } from './dto/update-facility-type.dto';
 
+/** Entities */
+import { FacilityType } from './entities/facility-type.entity';
+
+/** Services */
+import { RolesService } from 'src/security/roles/roles.service';
+
 @Injectable()
 export class FacilityTypesService {
-  create(createFacilityTypeDto: CreateFacilityTypeDto) {
-    return 'This action adds a new facilityType';
+  constructor(
+    @InjectRepository(FacilityType)
+    private readonly facilityTypeRepository: Repository<FacilityType>,
+    private readonly rolesService: RolesService,
+  ) {}
+
+  async create(
+    createFacilityTypeDto: CreateFacilityTypeDto,
+  ): Promise<FacilityType> {
+    const facilityType = this.facilityTypeRepository.create({
+      name: createFacilityTypeDto.name,
+      description: createFacilityTypeDto.description,
+    });
+    facilityType.responsible_roles = await this.rolesService.findByIds(
+      createFacilityTypeDto.responsible_roles,
+    );
+
+    return await this.facilityTypeRepository.save(facilityType);
   }
 
-  findAll() {
-    return `This action returns all facilityTypes`;
+  async findAll(): Promise<FacilityType[]> {
+    return await this.facilityTypeRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} facilityType`;
+  async findOne(id: number): Promise<FacilityType> {
+    return await this.facilityTypeRepository.findOneBy({ id });
   }
 
-  update(id: number, updateFacilityTypeDto: UpdateFacilityTypeDto) {
-    return `This action updates a #${id} facilityType`;
+  async findByName(name: string): Promise<FacilityType> {
+    return await this.facilityTypeRepository.findOne({ where: { name } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} facilityType`;
+  async update(
+    id: number,
+    updateFacilityTypeDto: UpdateFacilityTypeDto,
+  ): Promise<FacilityType> {
+    const facilityType = await this.findOne(id);
+    if (!facilityType) throw new NotFoundException('Facility Type not found.');
+
+    facilityType.name = updateFacilityTypeDto.name;
+    facilityType.description = updateFacilityTypeDto.description;
+    facilityType.responsible_roles = await this.rolesService.findByIds(
+      updateFacilityTypeDto.responsible_roles,
+    );
+    return await this.facilityTypeRepository.save(facilityType);
+  }
+
+  async remove(id: number): Promise<void> {
+    const facilityType = await this.findOne(id);
+    if (!facilityType) throw new NotFoundException('Facility Type not found.');
+
+    await this.facilityTypeRepository.remove(facilityType);
   }
 }
