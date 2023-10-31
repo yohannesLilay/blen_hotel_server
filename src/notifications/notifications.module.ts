973ad70ff8f0de43003556324859ec12cdeee6d2
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 10:44:01 +0300

    feat: Enhance NotificationsModule with TypeOrmModule and additional dependencies
    This commit enhances the NotificationsModule by incorporating TypeOrmModule for managing the Notification entity. The module now imports Notification entity and custom validators.

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/** Services */
 import { NotificationsService } from './notifications.service';
+
+/** Controllers */
 import { NotificationsController } from './notifications.controller';
 
+/** Entities */
+import { Notification } from './entities/notification.entity';
+
+/** Modules */
+import { UsersModule } from 'src/security/users/users.module';
+
+/** Custom Validations */
+import { ValidRecipientValidator } from './validators/valid-recipient.validator';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification]), UsersModule],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, ValidRecipientValidator],
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
