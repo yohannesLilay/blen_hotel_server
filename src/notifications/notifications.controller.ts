2a8406abecf968b46698f285dbfc8a64ef342e4d
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 10:43:16 +0300

    refactor: Update NotificationsController to include guards and query parameters
    This commit refactors the NotificationsController to include access token and permissions guards for enhanced security. It also adds query parameters for improved functionality, and the `markAsRead` endpoint for batch marking notifications as read.

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,20 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+  Query,
+  ParseBoolPipe,
+} from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
 
+/** Guards and Decorators */
+import { AccessTokenGuard } from 'src/security/auth/guards/access-token.guard';
+import { PermissionsGuard } from 'src/security/auth/guards/permissions.guard';
+import { Permissions } from 'src/security/auth/decorators/permissions.decorator';
+import { User } from 'src/security/auth/decorators/user.decorator';
+
+@UseGuards(AccessTokenGuard, PermissionsGuard)
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @Post()
-  create(@Body() createNotificationDto: CreateNotificationDto) {
-    return this.notificationsService.create(createNotificationDto);
-  }
-
   @Get()
-  findAll() {
-    return this.notificationsService.findAll();
+  findAll(
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('limit', ParseIntPipe) limit = 10,
+    @Query('excludeRead', ParseBoolPipe) excludeRead = true,
+    @User('id', ParseIntPipe) userId: number,
+  ) {
+    return this.notificationsService.findAll(page, limit, excludeRead, +userId);
   }
 
   @Get(':id')
@@ -22,12 +38,13 @@ export class NotificationsController {
     return this.notificationsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNotificationDto: UpdateNotificationDto) {
-    return this.notificationsService.update(+id, updateNotificationDto);
+  @Patch('mark-as-read')
+  markAsRead(@Body() notificationIds: number[]) {
+    return this.notificationsService.markAsRead(notificationIds);
   }
 
   @Delete(':id')
+  @Permissions('delete_notification')
   remove(@Param('id') id: string) {
     return this.notificationsService.remove(+id);
   }
