6b847144af4b89256008aeca3adddf2a420ee3bd
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-31 10:41:05 +0300

    feat: Add validation decorators to CreateNotificationDto
    This commit enhances the CreateNotificationDto class by adding validation decorators from class-validator library. These decorators ensure that the incoming data adheres to specific constraints and validation rules:
    These validations improve the data integrity and reliability of the CreateNotificationDto input, ensuring that only valid data is accepted when creating a new notification.

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,26 @@
-export class CreateNotificationDto {}
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Min,
+  Validate,
+} from 'class-validator';
+import { NotificationType } from '../constants/notification-type.enum';
+import { ValidRecipientValidator } from '../validators/valid-recipient.validator';
+
+export class CreateNotificationDto {
+  @IsNotEmpty()
+  @IsString()
+  message: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Validate(ValidRecipientValidator)
+  recipient: number;
+
+  @IsNotEmpty()
+  @IsEnum(NotificationType)
+  notification_type: NotificationType;
+}
