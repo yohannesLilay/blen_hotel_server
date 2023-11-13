6603ce15aefc57b595b5434d3baf91610425caba
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-13 05:08:40 +0300

    refactor: Update StoreRequisition entity with columns
    Modified the structure of the StoreRequisition entity class by adding more details:
    - Introduced a primary generated column for ID.
    - Added a unique store_requisition_number column with length and type specifications.
    - Included a store_requisition_date column of type Date.
    - Established a One-to-Many relationship with StoreRequisitionItem, allowing for cascading operations.
    - Incorporated Many-to-One relationships with User entities for requested_by and approved_by.
    - Introduced a status column with default value from the StoreRequisitionStatus enum.
    - Included created_at and updated_at columns for timestamps using CreateDateColumn and UpdateDateColumn.

--- src/sales/store-requisitions/entities/store-requisition.entity.ts
+++ src/sales/store-requisitions/entities/store-requisition.entity.ts
@@ -1,1 +1,50 @@
-export class StoreRequisition {}
+import {
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { StoreRequisitionItem } from './store-requisition-item.entity';
+import { User } from 'src/security/users/entities/user.entity';
+import { StoreRequisitionStatus } from '../constants/store-requisition-status.enum';
+
+export class StoreRequisition {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 20, unique: true })
+  store_requisition_number: string;
+
+  @Column()
+  store_requisition_date: Date;
+
+  @OneToMany(() => StoreRequisitionItem, (item) => item.store_requisition, {
+    cascade: true,
+  })
+  items: StoreRequisitionItem[];
+
+  @ManyToOne(() => User)
+  requested_by: User;
+
+  @ManyToOne(() => User, { nullable: true })
+  approved_by: User;
+
+  @Column({
+    type: 'enum',
+    enum: StoreRequisitionStatus,
+    default: StoreRequisitionStatus.REQUESTED,
+  })
+  status: StoreRequisitionStatus;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
