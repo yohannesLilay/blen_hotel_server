2c38f9cccad57664b94fa5c8ae015f2df87316f3
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-13 05:14:02 +0300

    refactor: Expand StoreRequisitionsModule with dependencies and feature modules
    Enhanced StoreRequisitionsModule to improve modularity and include necessary dependencies. Changes include:
    - Imported TypeOrmModule and specified entities for StoreRequisition and StoreRequisitionItem.
    - Added feature modules (ProductsModule, NotificationsModule, and WorkFlowsModule) for improved organization and encapsulation.
    - Included custom validators (UniqueStoreRequisitionNumberValidator and ValidProductValidator) and WebSocketsGateway.
    - Exported StoreRequisitionsService for potential use in other modules.
    - Updated controllers and services with the new dependencies.

--- src/sales/store-requisitions/store-requisitions.module.ts
+++ src/sales/store-requisitions/store-requisitions.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/** Services */
 import { StoreRequisitionsService } from './store-requisitions.service';
+
+/** Controllers */
 import { StoreRequisitionsController } from './store-requisitions.controller';
 
+/** Entities */
+import { StoreRequisition } from './entities/store-requisition.entity';
+import { StoreRequisitionItem } from './entities/store-requisition-item.entity';
+
+/** Modules */
+import { ProductsModule } from 'src/product-management/products/products.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
+import { WorkFlowsModule } from 'src/configurations/work-flows/work-flows.module';
+
+/** Custom Validators */
+import { UniqueStoreRequisitionNumberValidator } from './validators/unique-store-requisition-number.validator';
+import { ValidProductValidator } from './validators/valid-product.validator';
+
+/** Gateways */
+import { WebSocketsGateway } from 'src/web-sockets/web-sockets.gateway';
+
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([StoreRequisition, StoreRequisitionItem]),
+    ProductsModule,
+    NotificationsModule,
+    WorkFlowsModule,
+  ],
   controllers: [StoreRequisitionsController],
-  providers: [StoreRequisitionsService],
+  providers: [
+    StoreRequisitionsService,
+    UniqueStoreRequisitionNumberValidator,
+    ValidProductValidator,
+    WebSocketsGateway,
+  ],
+  exports: [StoreRequisitionsService],
 })
 export class StoreRequisitionsModule {}
