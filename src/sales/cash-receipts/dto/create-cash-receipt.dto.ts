e5ecc334acd5db90905f3304b9870ab4796776a7
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-20 14:17:34 +0300

    feat: add CreateCashReceiptDto class and related components
    - Introduce CreateCashReceiptDto class for representing the structure of a new cash receipt
    - Define validation decorators for properties like cash_receipt_number, cash_receipt_date, waiter, captain_order_ids, and items
    - Utilize class-validator and class-transformer decorators for input validation and transformation
    - Include custom validators such as ValidMenuValidator, UniqueCashReceiptNumberValidator, ValidStaffValidator, and ValidCaptainOrdersValidator for specific validations

---
+++ src/sales/cash-receipts/dto/create-cash-receipt.dto.ts
@@ -0,0 +1,68 @@
+import {
+  ArrayMinSize,
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsDateString,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+  Validate,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ValidMenuValidator } from '../validators/valid-menu.validator';
+import { UniqueCashReceiptNumberValidator } from '../validators/unique-cash-receipt-number.validator';
+import { ValidStaffValidator } from '../validators/valid-staff.validator';
+import { ValidCaptainOrdersValidator } from '../validators/valid-captain-orders.validator';
+
+class CashReceiptItemDto {
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  unit_price: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  @Validate(ValidMenuValidator)
+  menu_id: number;
+}
+
+export class CreateCashReceiptDto {
+  @IsString()
+  @IsNotEmpty()
+  @Validate(UniqueCashReceiptNumberValidator)
+  cash_receipt_number: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  cash_receipt_date: Date;
+
+  @IsInt()
+  @IsNotEmpty()
+  @Validate(ValidStaffValidator)
+  waiter: number;
+
+  @IsNotEmpty({ message: 'Captain Orders are required' })
+  @IsArray({ message: 'Captain Orders must be an array' })
+  @ArrayNotEmpty({ message: 'Captain Orders array cannot be empty' })
+  @ArrayUnique({
+    message: 'Captain Orders array cannot contain duplicate values',
+  })
+  @IsInt({ each: true, message: 'Each captain order ID must be an integer' })
+  @Min(1, { each: true, message: 'Each captain order ID must be at least 1' })
+  @Validate(ValidCaptainOrdersValidator)
+  captain_order_ids: number[];
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CashReceiptItemDto)
+  @ArrayMinSize(1, { message: 'At least one item is required' })
+  items: CashReceiptItemDto[];
+}
