5dfd64c746db8b4f1ef5bb65381d43e8237d6bb2
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-24 11:45:50 +0300

    feat: Add Room entity with TypeORM decorators
    - Define Room entity with TypeORM decorators for database mapping.
    - Includes columns for id, name, price, room_type, available, status, created_at, and updated_at.
    - Use 'enum' type for room_type column based on the RoomType enum.
    - Set default values for 'available' and 'status' columns.
    - Add created_at and updated_at columns with appropriate date types and default values.

---
+++ src/configurations/rooms/entities/room.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { RoomType } from '../constants/room_type.enum';
+
+@Entity()
+export class Room {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 10, unique: true })
+  name: string;
+
+  @Column({ type: 'int' })
+  price: number;
+
+  @Column({ type: 'enum', enum: RoomType })
+  room_type: RoomType;
+
+  @Column({ default: true })
+  available: boolean;
+
+  @Column({ default: true })
+  status: boolean;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
