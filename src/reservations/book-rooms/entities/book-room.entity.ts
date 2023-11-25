002d2bdfaf530b84eee5b7dda86707ef5fb62ec3
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-25 10:34:14 +0300

    feat: Add BookRoom entity with TypeORM decorators
    - Define BookRoom entity with TypeORM decorators for database mapping.
    - Includes columns for id, book_date, room, operator, created_at, and updated_at.
    - Set default values for 'book_date' columns.
    - Add created_at and updated_at columns with appropriate date types and default values.

---
+++ src/reservations/book-rooms/entities/book-room.entity.ts
@@ -0,0 +1,38 @@
+import { Room } from 'src/configurations/rooms/entities/room.entity';
+import { User } from 'src/security/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class BookRoom {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  book_date: Date;
+
+  @ManyToOne(() => Room)
+  room: Room;
+
+  @ManyToOne(() => User)
+  operator: User;
+
+  @Column({ type: 'varchar', nullable: true })
+  notes: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
