c9e9cd83eb12009be24a38f3465fc44aacdfd6ee
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-12-02 11:26:22 +0300

    feat: Set WebSocketGateway CORS origin dynamically
    Set the CORS origin for the WebSocketGateway in the @WebSocketGateway decorator to take its value from the 'CORS_ORIGIN' environment variable. This allows for dynamic configuration of the CORS origin, improving flexibility in deployment environments.

--- src/web-sockets/web-sockets.gateway.ts
+++ src/web-sockets/web-sockets.gateway.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: '*' })
+@WebSocketGateway({ cors: { origin: process.env.CLIENT_URL || '*' } })
 @Injectable()
 export class WebSocketsGateway implements OnGatewayDisconnect {
   @WebSocketServer() server: Server;
